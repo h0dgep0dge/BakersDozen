99c746f15c2371ab213d6fe0ffd7b49a83c0357b
Rob Mckennie <rob.t.mckennie@gmail.com>
2019-01-06 21:17:20 +1300

    Created an inkling of a concept

---
+++ wheat.js
@@ -0,0 +1,69 @@
+function plot(fert) {
+    this.state = 0;
+        // State
+        //  0 growing
+        //  1 grown, ready to harvest()
+        //  2 empty, needs sew()ing
+    this.fertility = fert; // Number of ticks to fully grow
+    this.growth = 0;
+    this.yeild = 3; // Yeild constant, the maximum grains harvested
+
+    this.tick = function() {
+        if(this.state == 0) {
+            this.growth += 1;
+            if(this.growth >= this.fertility) this.state = 1;
+        }
+    }
+
+    this.harvest = function() {
+        // This function returns the number of grains to add to the total
+        if(this.state == 1) {
+            this.growth = 0;
+            this.state = 2;
+            return Math.floor((Math.random() * this.yeild) + 1);;
+        }
+        return 0;
+    }
+}
+
+var grains = 0;
+var g = document.createElement("div");
+var plots = [new plot(10),new plot(11),new plot(12)];
+var p = document.createElement("div");
+function redraw() {
+    g.innerHTML = "Grains: "+grains.toString();
+    p.innerHTML = "";
+    plots.forEach(function(e) {
+        var progress = document.createElement("progress");
+        progress.max = e.fertility;
+        progress.value = e.growth;
+        p.appendChild(progress);
+        p.innerHTML += "<br>";
+    });
+}
+window.setInterval(function() {
+    plots.forEach(function(e) {e.tick();});
+    redraw();
+},1000);
+var b = document.createElement("button");
+b.innerHTML = "Harvest";
+b.addEventListener("click",function() {
+    plots.forEach(function(e) {grains += e.harvest();});
+    redraw();
+});
+var s = document.createElement("button");
+s.innerHTML = "Re-sew";
+s.addEventListener("click",function() {
+    plots.forEach(function(e) {
+        if(grains > 0 && e.state == 2) {
+            e.state = 0;
+            grains--;
+        }
+    });
+    redraw();
+});
+document.body.appendChild(g);
+document.body.appendChild(b);
+document.body.appendChild(s);
+document.body.appendChild(p);
+redraw();
